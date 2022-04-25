827cf5b23f28db31d85c5ab617ba15bc1a040a9a
Patrick Pluequett <ppluequett@gmail.com>
2022-04-25 15:06:44 +0200

    add basic structure for protected routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import AuthenticationButton from './components/AuthenticationButton';
 import LogoutButton from './components/LogoutButton';
 import SignupButton from './components/SignUpButton';
-
+import { Routes, Route } from 'react-router-dom';
+import UserPage from './components/UserPage';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import LoginButton from './components/LoginButton';
 function App() {
   return (
     <div className="App">
       <SignupButton />
-      <AuthenticationButton />
+      <LoginButton />
       <LogoutButton />
+      <Routes>
+        <Route
+          path="/loggedIn"
+          element={<ProtectedRoute component={UserPage} />}
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/UserPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserPage() {
+  return (
+    <div>
+      <h1>You are an user!</h1>
+    </div>
+  );
+}
+
+export default UserPage;
