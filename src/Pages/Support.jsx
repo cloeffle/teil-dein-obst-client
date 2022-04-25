e68f1ce93b491fc291d1fe059d71d1da8560e027
Lina Ho <yenthi.ho.lina@gmail.com>
2022-04-25 18:37:30 +0200

    fixed hamburger menu and added margin to support component

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -6,6 +6,12 @@ import "../Assets/styles/support.css";
 import Logo from "../Assets/logo/Logo.svg";
 
 export default function Support() {
+  const [userInput, setUserInput] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
   let navigate = useNavigate();
   function goBack() {
     navigate("/");
@@ -34,53 +40,69 @@ export default function Support() {
   };
   return (
     <>
-      <div className="toMap">
-        <button className="toMap-btn" onClick={goBack}>
-          zurück
-        </button>
-      </div>
-      <div className="logo">
-        <img src={Logo} alt="logo" />
-      </div>
-      <div className="support-container">
-        <form ref={form} onSubmit={sendEmail} className="support-form">
-          <h3>Kontaktformular</h3>
-          <div className="support-content">
-            Du hast Fragen, Anregungen oder Dein Obstbaum wurde
-            fälschlicherweise hier hochgeladen?
-            <br />
-            Dann kontaktiere uns gerne über dieses Kontaktformular. Wir werden
-            uns schnellstmöglich mit Dir in Verbindung setzen!
-          </div>
-          <label>Name</label>
-          <input
-            type="text"
-            name="user_name"
-            className="support-name"
-            placeholder="Gebe hier deinen Namen ein"
-            required
-          />
-          <label>E-Mail Adresse</label>
-          <input
-            type="email"
-            name="user_email"
-            className="support-mail"
-            placeholder="Gebe hier deine E-Mail Adresse ein"
-            required
-          />
-          <label>Deine Nachricht an uns</label>
-          <textarea
-            className="support-message"
-            name="message"
-            cols="30"
-            rows="8"
-            placeholder="Deine Nachricht..."
-            required
-          ></textarea>
-          <button className="support-submit" type="submit" value="ABSCHICKEN">
-            ABSCHICKEN
+      <div className="supportWrapper">
+        <div className="toMap">
+          <button className="toMap-btn" onClick={goBack}>
+            zurück
           </button>
-        </form>
+        </div>
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="support-container">
+          <form ref={form} onSubmit={sendEmail} className="support-form">
+            <h3>Kontaktformular</h3>
+            <div className="support-content">
+              Du hast Fragen, Anregungen oder Dein Obstbaum wurde
+              fälschlicherweise hier hochgeladen?
+              <br />
+              Dann kontaktiere uns gerne über dieses Kontaktformular. Wir werden
+              uns schnellstmöglich mit Dir in Verbindung setzen!
+            </div>
+            <label>Name</label>
+            <input
+              type="text"
+              name="user_name"
+              className="support-name"
+              placeholder="Gebe hier Deinen Namen ein"
+              required
+              onChange={(e) =>
+                setUserInput({ ...userInput, name: e.target.value })
+              }
+            />
+            <label>E-Mail Adresse</label>
+            <input
+              type="email"
+              name="user_email"
+              className="support-mail"
+              placeholder="Gebe hier Deine E-Mail Adresse ein"
+              required
+              onChange={(e) =>
+                setUserInput({ ...userInput, email: e.target.value })
+              }
+            />
+            <label>Deine Nachricht an uns</label>
+            <textarea
+              className="support-message"
+              name="message"
+              cols="30"
+              rows="8"
+              placeholder="Deine Nachricht..."
+              required
+              onChange={(e) =>
+                setUserInput({ ...userInput, message: e.target.value })
+              }
+            ></textarea>
+            <button
+              className="support-submit"
+              type="submit"
+              value="ABSCHICKEN"
+              disabled={!userInput}
+            >
+              ABSCHICKEN
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
