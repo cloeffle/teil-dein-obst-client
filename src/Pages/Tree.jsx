bccaf0a32328b827c88678995b1e531165ad8088
Patrick Pluequett <ppluequett@gmail.com>
2022-04-26 14:59:45 +0200

    get user infos

--- src/Pages/Tree.jsx
+++ src/Pages/Tree.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Tree() {
-  return <div>Tree</div>;
+  const { user } = useAuth0();
+  console.log(user);
+  return <div>He</div>;
 }
 
 export default Tree;
