5c1d28acd366e55a8f05ebe0fc867db59b33d306
Christian Löffler <cloeffle74@gmail.com>
2022-04-26 16:41:17 +0200

    changeing chards

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,10 +11,11 @@ import Kirsche from '../assets/images/icons8-cherry-500.png';
 import Stachelbeere from '../assets/images/icons8-gooseberry-500.png';
 import Weintraube from '../assets/images/icons8-grapes-500.png';
 import Pflaume from '../assets/images/icons8-plum-500.png';
+import Korb from '../assets/images/fruit basket 500.png';
 
 function Filter() {
   return (
-    <div>
+    <div className="container">
       <div className="chooseBtnCont">
         <button className="chooseBtn">
           <p>ALLE:</p>
@@ -33,85 +34,79 @@ function Filter() {
         </button>
       </div>
 
-      <div className="container">
-        <div>
+      <div className="cardContainer">
+        <div className="row">
           <button className="card-2">
             <img src={Apfel} alt="Apfel" />
             <p>Äpfel</p>
           </button>
-        </div>
-        <div>
           <button className="card-2">
             <img src={Aprikose} alt="Aprikose" />
             <p>Aprikosen</p>
           </button>
         </div>
-        <div>
+
+        <div className="row">
           <button className="card-2">
             <img src={Birne} alt="Birne" />
             <p>Birnen</p>
           </button>
-        </div>
-        <div>
           <button className="card-2">
             <img src={Erdbeere} alt="Erdbeeren" />
             <p>Erdbeeren</p>
           </button>
         </div>
-        <div>
+
+        <div className="row">
           <button className="card-2">
             <img src={Heidelbeere} alt="Heidelbeere" />
             <p>Heidelbeeren</p>
           </button>
-        </div>
-        <div>
           <button className="card-2">
             <img src={Himbeere} alt="Himbeere" />
             <p>Himbeere</p>
           </button>
         </div>
-        <div>
+
+        <div className="row">
           <button className="card-2">
             <img src={Johannisbeere} alt="Johannisbeere" />
             <p>Johannisbeeren</p>
           </button>
-        </div>
-        <div>
           <button className="card-2">
             <img src={Kirsche} alt="Kirschen" />
             <p>Kirschen</p>
           </button>
         </div>
-        <div>
+
+        <div className="row">
           <button className="card-2">
             <img src={Stachelbeere} alt="Stachelbeeren" />
             <p>Stachelbeeren</p>
           </button>
-        </div>
-        <div>
           <button className="card-2">
             <img src={Pflaume} alt="Pflaumen" />
             <p>Pflaumen</p>
           </button>
         </div>
-        <div>
+
+        <div className="row">
           <button className="card-2">
             <img src={Weintraube} alt="Weintrauben" />
             <p>Weintrauben</p>
           </button>
-        </div>
-        <div>
           <button className="card-2">
-            <img src="#" alt="Sonstiges" />
+            <img src={Korb} alt="Sonstiges" />
             <p>Sonstiges</p>
           </button>
         </div>
-        <div>
-          <button className="select">Filter anwenden</button>
-        </div>
-        <div>
-          <button className="delete">Filter löschen</button>
-        </div>
+      </div>
+
+      <div>
+        <button className="select">Filter anwenden</button>
+      </div>
+      <div>
+        <button className="delete">Filter löschen</button>
       </div>
     </div>
   );
