552b958c974dbbe0992a17967e5a74a4820780af
Lina Ho <yenthi.ho.lina@gmail.com>
2022-05-06 15:19:08 +0200

    comment shows up directly after posting

--- src/Pages/LocationDetails.jsx
+++ src/Pages/LocationDetails.jsx
@@ -1,50 +1,47 @@
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import Logo from '../assets/logo/Logo.svg';
-import Login from '../components/Login/LoginButton';
-import Apfel from '../assets/images/icons8-apple-500.png';
-import Aprikose from '../assets/images/icons8-apricot-500.png';
-import Blaubeere from '../assets/images/icons8-blueberry-500.png';
-import Kirsche from '../assets/images/icons8-cherry-500.png';
-import Weintraube from '../assets/images/icons8-grapes-500.png';
-import Pflaume from '../assets/images/icons8-plum-500.png';
-import Erdbeere from '../assets/images/icons8-strawberry-500.png';
-import Sonstiges from '../assets/images/fruit basket 500.png';
-import Birne from '../assets/images/icons8-pear-500.png';
-import Like_black from '../assets/images/Like_black.png';
-import Like_red from '../assets/images/Like_red.png';
+import Logo from "../assets/logo/Logo.svg";
+import Login from "../components/Login/LoginButton";
+import Apfel from "../assets/images/icons8-apple-500.png";
+import Aprikose from "../assets/images/icons8-apricot-500.png";
+import Blaubeere from "../assets/images/icons8-blueberry-500.png";
+import Kirsche from "../assets/images/icons8-cherry-500.png";
+import Weintraube from "../assets/images/icons8-grapes-500.png";
+import Pflaume from "../assets/images/icons8-plum-500.png";
+import Erdbeere from "../assets/images/icons8-strawberry-500.png";
+import Sonstiges from "../assets/images/fruit basket 500.png";
+import Birne from "../assets/images/icons8-pear-500.png";
+import Like_black from "../assets/images/Like_black.png";
+import Like_red from "../assets/images/Like_red.png";
 
-import '../assets/styles/locationDetails.css';
+import "../assets/styles/locationDetails.css";
 
 function LocationDetails({ locationData }) {
   //GET LOCATION DATA FOR SPECIFIC LOCATION (ID)
-  console.log('locationData', locationData);
+  console.log("locationData", locationData);
   const params = useParams();
   let locationDetail = locationData.find(function (location) {
     return location._id === params.id;
   });
-  console.log('LOCATIONDETAIL', locationDetail);
+  console.log("LOCATIONDETAIL", locationDetail);
 
   //GET USER DATA FROM AUTH0
   const { user } = useAuth0();
   const [userData, setUserData] = useState([]);
-  console.log('USER', userData);
+  console.log("USER", userData);
   const [liked, setLiked] = useState(false);
 
   useEffect(() => {
     if (user) {
-      axios
-        .get(
-          `http://localhost:8000/user/${user.sub.slice(user.sub.length - 7)}`
-        )
+      axios(`http://localhost:8000/user/${user.sub.slice(user.sub.length - 7)}`)
         .then((res) => {
           setUserData(res.data);
           if (res.data.favorites.includes(locationDetail._id)) {
             setLiked(true);
-            console.log('faved');
+            console.log("faved");
           }
         })
         .catch((err) => {
@@ -97,24 +94,23 @@ function LocationDetails({ locationData }) {
       });
   };
 
-  //POST COMMENT
-  const [comment, setComment] = useState({
-    comment: '',
-    timestamp: '',
-    user: '',
-    tree: '',
-    avatar: '',
-  });
-
   //GET COMMENTS
   const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:8000/comment/${locationDetail._id}`)
+    axios(`http://localhost:8000/comment/${locationDetail._id}`)
       .then((res) => setCommentList(res.data))
       .catch((err) => console.log(err));
   }, [locationDetail]);
+
+  //POST COMMENT
+  const [comment, setComment] = useState({
+    comment: "",
+    timestamp: "",
+    user: "",
+    tree: "",
+    avatar: "",
+  });
 
   let timestamp = new Date().toGMTString();
 
@@ -131,22 +127,24 @@ function LocationDetails({ locationData }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Comment', comment);
+    console.log("Comment", comment);
     axios
-      .post('http://localhost:8000/comment/', comment)
+      .post("http://localhost:8000/comment/", comment)
       .then((res) => {
         console.log(res);
+      })
+      .then(() => {
+        axios(`http://localhost:8000/comment/${locationDetail._id}`).then(
+          (res) => setCommentList(res.data)
+        );
       })
       .catch((err) => {
         console.log(err);
       });
     e.target.reset();
-    axios
-      .get(`http://localhost:8000/comment/${locationDetail._id}`)
-      .then((res) => setCommentList(res.data));
   };
 
-  console.log(comment.comment);
+  console.log(comment);
 
   return (
     <div className="locationDetails">
@@ -161,31 +159,31 @@ function LocationDetails({ locationData }) {
       <div className="locationDetails-content">
         {locationDetail && (
           <>
-            {locationDetail.type[0] === 'Apfel' && (
+            {locationDetail.type[0] === "Apfel" && (
               <img src={Apfel} alt="Apfel-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Aprikose' && (
+            {locationDetail.type[0] === "Aprikose" && (
               <img src={Aprikose} alt="Aprikose-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Birne' && (
+            {locationDetail.type[0] === "Birne" && (
               <img src={Birne} alt="Birne-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Blaubeere' && (
+            {locationDetail.type[0] === "Blaubeere" && (
               <img src={Blaubeere} alt="Blaubeere-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Kirsche' && (
+            {locationDetail.type[0] === "Kirsche" && (
               <img src={Kirsche} alt="Kirsche-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Weintraube' && (
+            {locationDetail.type[0] === "Weintraube" && (
               <img src={Weintraube} alt="Weintraube-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Pflaume' && (
+            {locationDetail.type[0] === "Pflaume" && (
               <img src={Pflaume} alt="Pflaume-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Erdbeere' && (
+            {locationDetail.type[0] === "Erdbeere" && (
               <img src={Erdbeere} alt="Erdbeere-Icon" height={120} />
             )}
-            {locationDetail.type[0] === 'Sonstiges' && (
+            {locationDetail.type[0] === "Sonstiges" && (
               <img src={Sonstiges} alt="Obstkorb" height={120} />
             )}
             <div>{locationDetail.type[0]}</div>
